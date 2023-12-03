576ea8d5f71aec56e97d77d3429bcd46a60cb116
ienrum <azang0506@gmail.com>
2023-12-03 17:46:15 +0900

    🍱 : favicon ico 및 html open graph 설정

--- src/components/LoadingCat.vue
+++ src/components/LoadingCat.vue
@@ -4,6 +4,9 @@
   <Teleport to="body">
     <div
       class="fixed left-0 top-0 z-30 flex h-screen w-screen flex-grow items-center justify-center backdrop-blur-sm">
-      <img class="align-middle" src="/loading-cat.gif" alt="loading cat" /></div
+      <img
+        class="h-1/4 align-middle"
+        src="/loading-cat.gif"
+        alt="loading cat" /></div
   ></Teleport>
 </template>
