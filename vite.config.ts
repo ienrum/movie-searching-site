5df62abc2e69105cc32be8179f44a4f2ec0c7ba6
ienrum <azang0506@gmail.com>
2023-12-03 11:49:22 +0900

    🚀 : vercel 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +9,14 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: "@",
-        replacement: path.resolve(__dirname, "src"),
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
       },
     ],
+  },
+  server: {
+    proxy: {
+      '/api': { target: 'http://localhost:2999' },
+    },
   },
 });
